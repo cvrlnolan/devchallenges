c7acf04d3fd37e4893615d52f35529813be31eca
George Carl <blackhxxdini@gmail.com>
2023-12-06 14:54:15 +0100

    chore: configure tailwindcss theme

--- apps/image-uploader/app/page.tsx
+++ apps/image-uploader/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Page(): JSX.Element {
   return (
     <div className='flex h-[100vh] w-full items-center justify-center'>
-      <p className='text-2xl font-bold'>Welcome to DevChallenges</p>
+      <p className='text-primary text-2xl font-bold'>
+        Welcome to DevChallenges
+      </p>
     </div>
   );
 }
---
+++ apps/image-uploader/postcss.config.js
@@ -0,0 +1,1 @@
+module.exports = require('@devchallenges/tailwindcss/postcss.config');
