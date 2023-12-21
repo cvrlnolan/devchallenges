9ee165f36ab1a710c020649ab0ee4ffeaaa0a9d3
George Carl <blackhxxdini@gmail.com>
2023-12-21 21:20:07 +0100

    feat(ui): add sheet ui component

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -4,6 +4,7 @@ export * from './dialog';
 export * from './input';
 export * from './label';
 export * from './navigation-menu';
+export * from './sheet';
 export * from './toast';
 export * from './toaster';
 export * from './use-toast';
