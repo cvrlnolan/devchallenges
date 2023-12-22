f78ea9f391d7e3ac62670f772eccf6952f01d6cf
George Carl <blackhxxdini@gmail.com>
2023-12-22 12:04:29 +0100

    chore(catwiki): setup catwiki app devchallenge

---
+++ apps/catwiki/app/layout.tsx
@@ -0,0 +1,25 @@
+import './globals.css';
+
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Catwiki',
+  description: 'devchallenges - Catwiki challenge',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}): JSX.Element {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>
+        <main className='container'>{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ apps/catwiki/next.config.js
@@ -0,0 +1,2 @@
+/** @type {import('next').NextConfig} */
+module.exports = require('@devchallenges/nextjs/next.config');
